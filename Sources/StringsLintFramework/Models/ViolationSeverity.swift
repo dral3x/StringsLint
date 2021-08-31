b572c6f4414deaf50362185a9e25c9462e829f5a
Alessandro Calzavara <alessandro.calzavara@gmail.com>
2021-08-31 10:21:34 +0200

    rename violation security "note" to "none", and filter those out when linting

--- Sources/StringsLintFramework/Models/ViolationSeverity.swift
+++ Sources/StringsLintFramework/Models/ViolationSeverity.swift
@@ -8,14 +8,17 @@
 import Foundation
 
 public enum ViolationSeverity: String, Comparable {
-    case note
+    /// Ignored. No report will contain any violation of this severity.
+    case none
+    /// Non-fatal. If using StringsLint as an Xcode build phase, Xcode will mark the build as having succeeded.
     case warning
+    /// Fatal. If using StringsLint as an Xcode build phase, Xcode will mark the build as having failed.
     case error
 }
 
 // MARK: Comparable
 public func < (lhs: ViolationSeverity, rhs: ViolationSeverity) -> Bool {
-    return lhs == .note && rhs == .warning ||
-        lhs == .note && rhs == .error ||
+    return lhs == .none && rhs == .warning ||
+        lhs == .none && rhs == .error ||
         lhs == .warning && rhs == .error
 }
