7c8e9ff9958b720bd33415aec7f9ccd4d446232c
Davide <info@davidebenini.it>
2026-01-09 15:34:37 +0100

    Improved keys matching (#40)
    * Manage string keys with interpolation, manage space before ignore command and other edge cases
    * Moved tests to SwiftParser tests
    
    ---------
    
    Co-authored-by: Davide Benini <davide.benini@spreaker.com>

--- Tests/StringsLintFrameworkTests/Rules/Lint/UnusedRuleTests.swift
+++ Tests/StringsLintFrameworkTests/Rules/Lint/UnusedRuleTests.swift
@@ -5,44 +5,40 @@
 //  Created by Alessandro "Sandro" Calzavara on 12/09/2018.
 //
 
-import XCTest
 @testable import StringsLintFramework
+import XCTest
 
 class UnusedRuleTests: XCTestCase {
-    
     func testUnusedExample() {
-        
         let file1 = File(name: "Localizable.strings", content: "\"abc\" = \"A B C\";")
         let file2 = File(name: "main.swift", content: "NSLocalizedString(\"def\", comment:\"\")")
-        
+
         let rule = UnusedRule()
         rule.processFile(file1)
         rule.processFile(file2)
-        
+
         XCTAssertEqual(rule.violations.count, 1)
         XCTAssertEqual(rule.violations[0].severity, .warning)
     }
-    
+
     func testBalancedUse() {
-        
         let file1 = File(name: "Localizable.strings", content: "\"loading\" = \"Loading...\";")
         let file2 = File(name: "main.swift", content: "NSLocalizedString(\"loading\", comment: \"\")")
-        
+
         let rule = UnusedRule()
         rule.processFile(file1)
         rule.processFile(file2)
-        
+
         XCTAssertEqual(rule.violations.count, 0)
     }
-    
+
     func testSeverityError() {
-        
         let file1 = File(name: "Localizable.strings", content: "\"abc\" = \"A B C\";")
-        
+
         let rule = UnusedRule()
         rule.severity = .error
         rule.processFile(file1)
-        
+
         XCTAssertEqual(rule.violations.count, 1)
         XCTAssertEqual(rule.violations[0].severity, .error)
     }
